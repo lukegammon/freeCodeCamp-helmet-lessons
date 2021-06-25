1e124f53f179a4947ab98f47acfb10de03b33fcb
Glitch (adhesive-chief-fisherman) <none>
2021-06-25 15:43:08 +0000

    🌠🎥 Checkpoint ./myApp.js:32528934/198

--- myApp.js
+++ myApp.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const helmet = require("helmet");
 
-app.use(helmet.hidePoweredBy()); // Hides that your backend is express.
-app.use(helmet.frameguard({action: "DENY"})); // Stops your page fropm being used in an iframe(click hijacking).
-app.use(helmet.xssFilter()); // basic protection from unwanted scripts being run(via input forms etc).
-app.use(helmet.noSniff()); // MIME type Sniffing sets the X-Content-Type-Options to noSniff.
-app.use(helmet.ieNoOpen()) // sets the X-Download-Options header to noopen. This will prevent IE users from executing downloads in the trusted site’s context.
+app.use(helmet.hidePoweredBy()); // Hides that your backend is express
+app.use(helmet.frameguard({action: "DENY"})); // Stops your page fropm being used in an iframe(click hijacking)
+app.use(helmet.xssFilter()); // basic protection from unwanted scripts being run(via input forms etc)
+app.use(helmet.noSniff()); // MIME type Sniffing sets the X-Content-Type-Options to noSniff
+app.use(helmet.ieNoOpen()) // sets the X-Download-Options header to noopen. This will prevent IE users from executing downloads in the trusted site’s context
 
 const ninetyDaysInSeconds = 90*24*60*60;
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true})) // force https for 90 days. protection against non secure(http) request. 
-app.use(helmet.dnsPrefetchControl()); // stop DNS prefectching o
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true})) // force https for 90 days. protection against non secure(http) request
+app.use(helmet.dnsPrefetchControl()); // stop DNS prefetching of records on page(links etc.)
+app.use(helmet.noCache()); // always download the newer version of a page, noCache tries to disable caching on client’s browser. 
 
 
 
